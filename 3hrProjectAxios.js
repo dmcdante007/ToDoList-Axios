3e9675eb6c001c973d2f20669bd427cf296113d4
dmcdante007 <meshramswaraj98>
2023-12-18 17:15:13 +0530

    2nd Project : ToDo List- 100% working (

--- 3hrProjectAxios.js
+++ 3hrProjectAxios.js
@@ -4,23 +4,23 @@ function saveDetailsinAPI(item){
     item.preventDefault();
 
     let obj={
-        "itemName": item.target.itemName.value,
+        "itemName": item.target.todoTask.value,
         "description": item.target.description.value,
-        "price": item.target.price.value,
-        "quantity": item.target.quantity.value,
     };
 
     console.log(obj)
-    axios.post("https://crudcrud.com/api/632ba6e0c770448d84009b8844c503bc/Data",obj)
+    axios.post("https://crudcrud.com/api/3e2dc51381fd4807858de261d4580b1a/data",obj)
     .then((response) => {
         console.log(response);
     }).catch((err) => {
         console.log(err)
     });
+
+    showItemonScreen(obj);
 }
 
 window.addEventListener("DOMContentLoaded", ()=>{  
-    axios.get("https://crudcrud.com/api/632ba6e0c770448d84009b8844c503bc/Data")  
+    axios.get("https://crudcrud.com/api/3e2dc51381fd4807858de261d4580b1a/data")  
     .then((response)=>{
         console.log(response)
         for( var i=0; i< response.data.length; i++){
@@ -39,45 +39,52 @@ function showItemonScreen(item) {
     //     "quantity": item.quantity
     // };
     const parentNode = document.getElementById("itemList");
-    const childHTML = `<li id= ${item._id}> ${item.itemName}     ${item.description}     ${item.price}rs     ${item.quantity}
-                                    <button onclick= whenUserBuys1('${item}')>Buy 1</button>
-                                    <button onclick= whenUserBuys2('${item}')>Buy 2</button>
+    const childHTML = `<li id= ${item._id}> ${item.itemName}  ${item.description}
+                                    <button onclick= Tick('${item._id}')>\u2713</button>
+                                    <button onclick= Remove('${item._id}')>&#10060</button>
                         </li>`
 
     parentNode.innerHTML = parentNode.innerHTML + childHTML;
 }
 
-function whenUserBuys1(item){
-    // item.preventDefault();
-    obj={
-        "_id": item._id,
-        "itemName": item.itemName,
-        "description": item.description,
-        "price": item.price,
-        "quantity": item.quantity,
-    }
-    
-    axios.put(`https://crudcrud.com/api/632ba6e0c770448d84009b8844c503bc/Data/${item}`, obj)
-    .then((response) => {
-        console.log(response);
-    }).catch((err) => {
-        console.log(err)
-    });
+
+function Tick(didTodo){
+    removeUserfromScreen(didTodo);
+    axios.get(`https://crudcrud.com/api/3e2dc51381fd4807858de261d4580b1a/data/${didTodo}`)  
+    .then((response)=>{
+        console.log(response)
+        showItemonTheDonelist(response.data);
+        
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
 }
 
-function whenUserBuys2(item){
-    a = itemquantity -2;
-    obj = {
-        "itemName": itemname,
-        "description": itemdes,
-        "price": itemprice,
-        "quantity": a
-    };
-    
-    axios.put("https://crudcrud.com/api/632ba6e0c770448d84009b8844c503bc/Data", obj )
-    .then((response) => {
-        console.log(response);
-    }).catch((err) => {
-        console.log(err)
-    });
+function showItemonTheDonelist(done){
+    const parentNode = document.getElementById("doneList");
+    const childHTML = `<li id= ${done._id}> ${done.itemName} ${done.description} - Compeleted</li>`
+
+    parentNode.innerHTML = parentNode.innerHTML + childHTML;
 }
+
+
+function removeUserfromScreen(userID){
+    const parentNode = document.getElementById('itemList');
+    const childNodetobeDeleted = document.getElementById(userID);
+    if (childNodetobeDeleted){
+        parentNode.removeChild(childNodetobeDeleted);
+    }
+}
+
+
+function Remove(X){
+    axios.delete(`https://crudcrud.com/api/3e2dc51381fd4807858de261d4580b1a/data/${X}`)
+    .then((response)=>{
+        removeUserfromScreen(X);
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+}
+
